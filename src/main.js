88034c156ef7ac3c12b19f7d97046c016b1d099a
SixZeros <526492409@qq.com>
2020-10-13 09:10:23 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import './styles.scss'
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
+
+Vue.use(ElementUI)
+
+createApp(App).use(store).use(router).mount('#app')
